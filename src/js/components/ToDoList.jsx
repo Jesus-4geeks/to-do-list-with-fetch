a3ed0781d4f2ef122bd23b2897867da6ee910f47
Jesus-4Geeks <madafaking@PC-JML.localdomain>
2025-08-12 05:23:40 +0200

    Fetch integrated

--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -1,26 +1,163 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ToDo from "./ToDo";
 
 const ToDoList = () => {
 	const [tasks, setTasks] = useState([]);
-	const [newId, setNewId] = useState(1);
+	const [newId, setNewId] = useState(0);
 	const [newTask, setNewTask] = useState("");
+
+	const playgroundAPI = "https://playground.4geeks.com/todo";
+	const myUsername = "jesus-4geeks";
 
 	const handleAddTask = (e) => {
 		if (e.key === "Enter" && newTask.trim()) {
-			setTasks((prevTasks) => [...prevTasks, { id: newId, value: newTask }]);
-			setNewId((prevId) => prevId + 1);
-			setNewTask(""); // Clears 'new-task' field
+			const todo = { id: newId, label: newTask, is_done: false };
+			setTasks((prevTasks) => [...prevTasks, todo]);
+			const ok = postTodo(todo);
+			
+			/* Si todo va bien, actualizamos las demás variables */
+			if (ok) {
+				setNewId((prevId) => prevId + 1);
+				setNewTask(""); // Clears 'new-task' field
+				/* alert('Todo creado con exito') */
+			}
+			else {
+				getTodos(); // Restaura los Todos
+			}
 		}
 	};
 
 	const handleDeleteTask = (idToDelete) => {
 		setTasks((prevTasks) => prevTasks.filter((task) => task.id !== idToDelete));
+		const ok = deleteTodo(idToDelete);
+
+		if (ok) {
+			/* alert('Todo eliminado con exito') */
+		}
+		else {
+			getTodos(); // Restaura los Todos
+		}
 	};
+
+	/* Métodos CRUD */
+
+	const postUser = async () => {
+		try {
+			const response = await fetch(`${playgroundAPI}/users/${myUsername}`, {
+				method: "POST"
+			})
+	
+			if (!response.ok) {
+				throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+			}
+
+		} catch (error) {
+			alert(`Hubo un error al crear el usuario ${myUsername}`);
+			console.error('error: ', error);
+		}
+		finally {
+			alert(`Usuario ${myUsername} creado con exito`)
+		}
+	};
+	
+	
+	const deleteUser = async () => {
+		try {
+			const response = await fetch(`${playgroundAPI}/users/${myUsername}`, {
+				method: "DELETE"
+			})
+			
+			if (!response.ok) {
+				throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+			}
+
+		} catch (error) {
+			alert(`Hubo un error al eliminar el usuario ${myUsername}`);
+			console.error('error: ', error);
+		}
+		finally {
+			setTasks([]);
+			setNewId(0);
+			alert('Usuario eliminado con exito')
+		}
+	};
+
+
+	const getTodos = async () => {
+		try {
+			const response = await fetch(`${playgroundAPI}/users/${myUsername}`);
+			let contentType = response.headers.get('content-type');
+
+			/* Si no existe el usuario, se crea y se vuelve a intentar el GET */
+			if (response.status === 404) {
+				postUser();
+				getTodos();
+				return;
+			}
+
+			if (!(response.ok && contentType && contentType.includes("application/json"))) {
+				throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+			}
+			
+			const data = await response.json();
+			setTasks(data.todos);
+			/* alert('Todos cargados con exito'); */
+			
+		} catch (error) {
+			alert('Hubo un error al cargar los Todos');
+			console.error('error: ', error);
+		}
+	};
+	
+	
+	const postTodo = async (todo) => {
+		try {
+			const response = await fetch(`${playgroundAPI}/todos/${myUsername}`, {
+				method: 'POST',
+				body: JSON.stringify(todo),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			
+			if (!response.ok) {
+				throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+			}
+			return response.ok;
+			
+		} catch (error) {
+			alert('Hubo un error al crear el nuevo Todo');
+			console.error('error: ', error);
+		}
+	};
+	
+	
+	const deleteTodo = async (todo_id) => {
+		try {
+			const response = await fetch(`${playgroundAPI}/todos/${todo_id}`, {
+				method: 'DELETE'
+			});
+			
+			if (!response.ok) {
+				throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+			}
+			return response.ok;
+			
+		} catch (error) {
+			alert(`Hubo un error al eliminar el Todo con id: ${todo_id}`);
+			console.error('error: ', error);
+		}
+	};
+
+
+	useEffect(() => {
+		/* Cargamos las tareas al inicio */
+		getTodos();
+	}, [])
 
 	return (
 		<div>
-			<h1>To-Do List</h1>
+			<h1>To-Do List (with Fetch)</h1>
 			<div id="to-do-list">
 
 				{/* Add task Bar */}
@@ -39,7 +176,7 @@ const ToDoList = () => {
 
 				{/* To-Do's */}
 				{tasks.map((task) => (
-					<ToDo key={task.id} value={task.value} deleteTask={() => handleDeleteTask(task.id)} />
+					<ToDo key={task.id} value={task.label} deleteTask={() => handleDeleteTask(task.id)} />
 				))}
 
 				{/* Items left */}
@@ -47,6 +184,15 @@ const ToDoList = () => {
 					{tasks.length === 0 ? "There are no tasks. Add tasks." : (
 						`${tasks.length} ${tasks.length > 1 ? "items" : "item"} left`
 					)}
+
+					<span className="ms-auto">
+						<button
+							className="btn btn-danger"
+							onClick={deleteUser}
+						>
+							Eliminar usuario
+						</button>
+					</span>
 				</div>
 			</div>
 		</div>
